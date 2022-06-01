968b6760d5be1b347cd603229c3b71a3e174a08f
vuongphamhd94 <vuongphamhd94@gmail.com>
2022-06-01 15:04:33 +0700

    update Sansi.

---
+++ src/AdminLTE/Controllers/Sansi/SansiHomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminLTE.Controllers.Sansi
+{
+    public class SansiHomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult IndexQuanLyDonHang()
+        {
+            return View();
+        }
+        public IActionResult IndexSuaDon()
+        {
+            return View();
+        }
+        public IActionResult IndexCaiDatThayDoiThongTin()
+        {
+            return View();
+        }
+        public IActionResult IndexTaoDonHangKyGui()
+        {
+            return View();
+        }
+        public IActionResult IndexTimKiemSanPham()
+        {
+            return View();
+        }
+    }
+}
