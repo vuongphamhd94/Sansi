fcfbd9d00aa57d823af5c9d1471163002179a936
vuongphamhd94 <vuongphamhd94@gmail.com>
2022-06-13 14:17:17 +0700

    update  20220615- update DB

--- src/AdminLTE/wwwroot/js/Sansi01.js
+++ src/AdminLTE/wwwroot/js/Sansi01.js
@@ -1,4 +1,6 @@
-﻿let tieuDe = document.querySelectorAll(".tim-kiem-sp-nav-top > div");
+﻿////const { Button } = require("bootstrap");
+
+let tieuDe = document.querySelectorAll(".tim-kiem-sp-nav-top > div");
 for (let i = 0; i < tieuDe.length; i++) {
     tieuDe[i].addEventListener("click", function () {
         if (i == 4) {
@@ -26,3 +28,33 @@ for (let i = 0; i < tieuDe.length; i++) {
     })
 }
 
+
+let bt = document.querySelector("#bt-sm");
+let sanPham = [];
+bt.addEventListener("click", function () {
+
+    // let newSanPham  = {
+    //    name: document.querySelector("#name").value,
+    //    title: document.querySelector("#image").value,
+    //    price: document.querySelector("#price")
+    //}
+    //sanPham.push(newSanPham);
+    //console.log(newSanPham);
+
+    
+    $.ajax({
+        url: "/SansiHome/AddDanhMucSanPham",
+        method: "POST",
+        data: {
+            /*Id: 2,*/
+            TenSanPham: "San Pham 01",
+            Type: 1,
+            GiaSanPham: 12.5,
+            UrlImage: "link o1"
+        }
+    }).done(function () {
+        console.log("Hoan thanh")
+    });
+});
+
+/*let url = "/cotrollerName/functionName";*/
